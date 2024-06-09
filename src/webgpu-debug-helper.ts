fef158070259ff7e746e261f5759df4a2412b4d0
Gregg Tavares <github@greggman.com>
2024-06-09 10:46:42 -0700

    Require loading as a module
    The issue is wgsl_reflect defines `class Function`. If this code
    is loaded as a non-module then that ends up overriding the default
    `Function` class and code that's checking `... instanceof Function`
    starts failing.
    
    There's no easy way for a script to check if it was loaded as a
    module or a non-module but using `import.meta` ends up requiring
    it to be loaded as a module.

--- src/webgpu-debug-helper.ts
+++ src/webgpu-debug-helper.ts
@@ -6,3 +6,7 @@ import './compute-pass-encoder.js';
 import './render-pass-encoder.js';
 import './render-bundle-encoder.js';
 import './texture.js';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const _ = import.meta; // webgpu-debug-helper must be imported as a module with import or `<script type="module" ...>`
+console.log('webgpu-debug-helper loaded:', _ !== undefined);
